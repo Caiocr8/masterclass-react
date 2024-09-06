0e31970c4c85fa61d8b97763028a00ae6219d274
Caio Rafael <ccaiocr@gmail.com>
2024-09-06 00:45:56 -0300

    first commit

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,17 @@
+interface TweetProps {
+  user: string
+  children: string
+  likes?: number
+}
+
+
+export function Tweet(props: TweetProps) {
+  console.log(props)
+  return (
+    <div className="tweet" style={{borderRadius: 8}}>
+      <strong>{props.user}</strong>
+      <p>{props.children}</p>
+      <button>Like {props.likes ?? 0}</button>
+    </div>
+  )
+}  
